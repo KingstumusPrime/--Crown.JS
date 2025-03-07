ae62283109138d219e6f3e88a363b6e0cda6cba1
KingstumusPrime <90068971+KingstumusPrime@users.noreply.github.com>
2025-03-07 11:57:38 -0500

    Update actor.js

--- src/actor.js
+++ src/actor.js
@@ -1,15 +1,17 @@
+let CROWN_DISABLE_INPUT = false;
 class Actor {
     constructor(name, sprite, context){
         this.name = name;
         // sprite is a reference to any class with a render function
         this.sprite = sprite;
         this.pos = {x: 0, y: 0};
+        this.sprSz = {x: 1, y: 1};
         // how many degrees of rotation
         this.rot = 0;
         this.scale = this.sprite.scale;
         this.ctx = context;
         this.culled = false;
-
+ 
         // update scale wih loaded sprite
         this.sprite.onload = () => {
             this.scale = this.sprite.scale;
@@ -20,13 +22,16 @@ class Actor {
 
     // for the user to fill out
     update(cam){
-        if(actorsCollides(this, cam)){
+        if(actorsCollides(this, cam) || (cam.culling == false)){
             this.culled = false;
+            this.ctx.save();
+            this.ctx.setTransform(this.sprSz.x, 0, 0, this.sprSz.y, ((this.pos.x - cam.pos.x)+ this.scale.x/2) * (1 - this.sprSz.x), ((this.pos.y - cam.pos.y) + this.scale.y/2) * (1 - this.sprSz.y));
             this.sprite.render(this.ctx, VecSub(this.pos, cam.pos));
+            this.ctx.restore();
         }else{
             this.culled = true;
         }
-
+        
         this.onUpdate();
     };
 
