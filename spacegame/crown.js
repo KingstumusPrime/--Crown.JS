dd783b1336ce987e5dd61a7b49d56a62e14b0320
KingstumusPrime <90068971+KingstumusPrime@users.noreply.github.com>
2025-03-07 12:03:52 -0500

    Update crown.js

--- spacegame/crown.js
+++ spacegame/crown.js
@@ -1,15 +1,17 @@
+let CROWN_DISABLE_INPUT = false;
 class Actor {
     constructor(name, sprite, context){
         this.name = name;
         // sprite is a reference to any class with a render function
         this.sprite = sprite;
         this.pos = {x: 0, y: 0};
+        this.sprSz = {x: 1, y: 1};
         // how many degrees of rotation
         this.rot = 0;
         this.scale = this.sprite.scale;
         this.ctx = context;
         this.culled = false;
-
+ 
         // update scale wih loaded sprite
         this.sprite.onload = () => {
             this.scale = this.sprite.scale;
@@ -20,13 +22,16 @@ class Actor {
 
     // for the user to fill out
     update(cam){
-        if(actorsCollides(this, cam)){
+        if(actorsCollides(this, cam) || (cam.culling == false)){
             this.culled = false;
+            this.ctx.save();
+            this.ctx.setTransform(this.sprSz.x, 0, 0, this.sprSz.y, ((this.pos.x - cam.pos.x)+ this.scale.x/2) * (1 - this.sprSz.x), ((this.pos.y - cam.pos.y) + this.scale.y/2) * (1 - this.sprSz.y));
             this.sprite.render(this.ctx, VecSub(this.pos, cam.pos));
+            this.ctx.restore();
         }else{
             this.culled = true;
         }
-
+        
         this.onUpdate();
     };
 
@@ -52,6 +57,7 @@ class Camera {
         this.target = {x: 0, y: 0};
         this.excel = {x: 0, y: 0};
         this.drag = 0.3;
+        this.culling = true;
     }
 
     update(){
@@ -107,7 +113,12 @@ class Game {
 
     addActor(name, sprite, params={}){
         const act = new Actor(name, sprite, this.ctx);
-        this.updaters.push(act);
+        if("bottom" in params && params["bottom"] == true){
+            this.updaters.unshift(act);
+        }else{
+            this.updaters.push(act);
+        }
+
         this.objMap[name] = act;
         if("group" in params){
             this.addToGroup(params["group"], act);
@@ -120,8 +131,14 @@ class Game {
         const act = new Actor(name, sprite, this.ctx);
         act.pos = pos;
         this.objMap[name] = act;
-        if(sprite.img !== undefined){
+
+        if(sprite.img !== undefined && sprite.loaded == false){
+            let prev = sprite.img.onload;
             sprite.img.onload = () => {
+                if(prev){
+                    prev();
+                }
+                
                 act.sprite.render(this.offCtx, act.pos, act.scale);
             }
         }else{
@@ -148,7 +165,7 @@ class Game {
         this.input.update();
         // update the camera
         this.camera.update();
-
+        this.ctx.drawImage(this.offScreenCanvas, -this.camera.pos.x, -this.camera.pos.y);
         // render all the static objects
         this.tweenList.forEach((tween, i) => {
             tween.step();
@@ -156,7 +173,7 @@ class Game {
                 delete this.tweenList[i];
             }
         }) 
-        this.ctx.drawImage(this.offScreenCanvas, -this.camera.pos.x, -this.camera.pos.y);
+
         this.updaters.forEach((updater => {
             if(updater.sprite.loaded != false){
                 updater.update(this.camera);
@@ -198,8 +215,8 @@ class Game {
         return this.groupMap[name] != undefined ? this.groupMap[name] : [];
     }
     
-    addTween(start, end, func, algo, endfunc){
-        this.tweenList.push(new TWEEN(start, end, func, algo, endfunc));
+    addTween(start, end, func, algo, endfunc, speed){
+        this.tweenList.push(new TWEEN(start, end, func, algo, endfunc, speed));
     }
 }class BoxGeometry {
     constructor(scale, color="black"){
@@ -219,12 +236,12 @@ class Game {
 }
 
 class RectGeometry {
-    constructor(x, y){
+    constructor(x, y, color="black"){
         this.loaded = true;
         this.opacity = 1;
         this.scale = {x: x, y:y};
         this.rot = 0;
-        this.color = "black";
+        this.color = color;
     }
 
     render(ctx, pos){
@@ -257,7 +274,6 @@ class Line{
         ctx.globalAlpha = this.opacity;
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.moveTo(pos.x, pos.y);
         console.log(this.rot);
         const angle = this.rot*Math.PI/180;
         this.start = pos;
@@ -267,6 +283,28 @@ class Line{
         ctx.restore();
     }
 }
+class CircleGeometry {
+    constructor(scale, color="black"){
+        this.loaded = true;
+        this.opacity = 1;
+        this.scale = {x: scale, y:scale};
+        this.color = color;
+    }
+
+    render(ctx, pos){
+        
+        ctx.save();
+        ctx.globalAlpha = this.opacity;
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.translate(pos.x + this.scale.x/2, pos.y + this.scale.y/2);
+        ctx.arc(0, 0, this.scale.x/2, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.globalAlpha = 1;
+        ctx.restore();
+    }
+}
+
 
 class Sprite {
     constructor(src, scale={x:0,y:0}){
@@ -274,6 +312,7 @@ class Sprite {
         this.img.src = src;
         this.scale = scale;
         this.opacity = 1;
+        this.rot = 0;
         this.loaded = false;
         this.img.addEventListener('load', () => {
             if(this.scale.x == 0 || this.scale.y == 0){
@@ -286,18 +325,53 @@ class Sprite {
     }
 
     render(ctx, pos){
+        ctx.save();
         ctx.globalAlpha = this.opacity;
-        ctx.drawImage(this.img, pos.x, pos.y, this.scale.x, this.scale.y);
+        ctx.fillStyle = this.color;
+    
+        // Translate to the center of the image
+        ctx.translate(pos.x + this.scale.x / 2, pos.y + this.scale.y / 2);
+    
+        // Rotate around the center
+        ctx.rotate(this.rot * Math.PI / 180);
+    
+        // Draw the image, offsetting by half the width and height
+        ctx.drawImage(this.img, -this.scale.x / 2, -this.scale.y / 2, this.scale.x, this.scale.y);
+    
         ctx.globalAlpha = 1;
+        ctx.restore();
     }
 
     // used to update actors when the image loads
     onload(){}
-}class InputController {
+}
+class TextSprite {
+    constructor(text, font="bold 16px Arial"){
+
+        this.text = text;
+        console.log(this.text);
+        this.color = "black";
+        this.opacity = 1;
+        this.font = font;
+        this.scale = {x: 100, y: 100};
+    }
+    render(ctx, pos){
+        ctx.globalAlpha = this.opacity;
+        ctx.font = this.font;
+    
+        let sz = ctx.measureText(this.text);
+        this.scale.y = sz.fontBoundingBoxAscent + sz.fontBoundingBoxDescent;  
+        this.scale.x = sz.width; 
+        ctx.fillText(this.text, pos.x, pos.y);
+    }
+}
+
+class InputController {
     constructor(){
         this.keyRepeatListeners = {};
         this.keyListeners = {};
         this.keys = {};
+        this.funcs = {};
         this.mouse = {x: 0, y: 0};
         this.mousePrev;
         this.prevKeys = null;
@@ -323,7 +397,19 @@ class Sprite {
         }
         const e = new CustomEvent(key);
         document.addEventListener(key,func)
+        this.funcs[key] = func;
         this.keyListeners[key] = e;
+    }
+
+    clearKey(key){
+        document.removeEventListener(key, this.funcs[key]);
+        this.keyListeners[key] = null;
+    }
+    clearKeys(keys){
+        keys.forEach((key)=>{
+            this.clearKey(key);
+        })
+
     }
 
     onKeyPressedRepeat(key, func){
@@ -341,7 +427,7 @@ class Sprite {
 
     onkeyDown(e){
         this.keys[e.key] = true;
-        if(this.keys[e.key] && !this.prevKeys[e.key] && !this.paused){
+        if(this.keys[e.key] && !this.prevKeys[e.key] && !this.paused && !CROWN_DISABLE_INPUT){
             if(this.keyListeners[e.key] != null){
                 document.dispatchEvent(this.keyListeners[e.key]);
             }
@@ -349,11 +435,12 @@ class Sprite {
     }
 
     update(){
+        if(this.paused){return;}
         this.prevKeys = {...this.keys};
         this.mousePrev = {...this.mouse};
         Object.keys(this.keyRepeatListeners).forEach(key => {
             // fire repeat events
-            if(this.keyRepeatListeners[key] != null && this.keys[key] && !this.paused){
+            if(this.keyRepeatListeners[key] != null && this.keys[key] && !this.paused && !CROWN_DISABLE_INPUT){
                 document.dispatchEvent(this.keyRepeatListeners[key]);
             }
         })
@@ -391,7 +478,11 @@ class Sprite {
         }
     }
 
-}function actorsCollides(act1, act2){
+}
+
+let CROWN_RANDOM_FUNC = Math.random;
+
+function actorsCollides(act1, act2){
     return act1.pos.y + act1.scale.y > act2.pos.y && act1.pos.y < act2.pos.y + act2.scale.y
     && act1.pos.x + act1.scale.x > act2.pos.x && act1.pos.x < act2.pos.x + act2.scale.x;
 }
@@ -402,11 +493,15 @@ function VecSub(v1, v2){
 }
 
 function randFromArr(arr){
-    return arr[Math.floor(Math.random()* arr.length)];
+    return arr[randInRange(0, arr.length)];
 }
 
 function randInRange(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(CROWN_RANDOM_FUNC() * (max - min)) + min;
+}
+
+function randFloatInRange(min, max){
+    return CROWN_RANDOM_FUNC() * (max - min) + min;
 }
 
 // creates a variable of name with a on update event
@@ -431,9 +526,8 @@ function numWithLength(num, len){
 function lerp(a, b, t){
     return a + (b-a)*t;
 }
-
 class TWEEN {
-    constructor(start, end, func, algo, afterFunc=undefined){
+    constructor(start, end, func, algo, afterFunc=undefined, rate=0.01){
         this.t = 0;
         this.start = start;
         this.end = end;
@@ -441,11 +535,13 @@ class TWEEN {
         this.algo = algo;
         this.finished = false;
         this.afterFunc = afterFunc;
+        this.rate = rate;
     }
 
     step(){
         if(!this.finished){
             if(this.t < 1){
+                this.t += this.rate; 
                 const fixedT = this.algo(this.t);
                 // lerp the values
                 const v = lerp(this.start, this.end, fixedT);
@@ -453,7 +549,8 @@ class TWEEN {
                 this.func(v, fixedT);
                 // use a built in or user defined function to modify t
                 // example ease in, elastic, constantRate, whatever else i add
-                this.t += 0.01; 
+
+                
             }else if(!this.finished){
                 this.finished = true;
                 if(typeof this.afterFunc === "function"){
